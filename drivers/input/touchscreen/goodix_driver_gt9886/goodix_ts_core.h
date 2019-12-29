2458924167670e5932d8b565411fc73e7ceb8456
Yaroslav Furman <yaro330@gmail.com>
2019-12-30 00:07:28 +0300

    goodix_driver_gt9886: silence debugging
    SHUT THE FUF UFF.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_core.h
+++ drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_core.h
@@ -703,10 +703,17 @@ static inline u32 checksum_be32(u8 *data, u32 size)
 #define EMEMCMP					1003
 
 /* log macro */
+#ifdef CONFIG_GOODIX_DEBUG
 #define ts_info(fmt, arg...)	pr_info("[GTP9886-INF][%s:%d] "fmt"\n", __func__, __LINE__, ##arg)
 #define	ts_err(fmt, arg...)		pr_err("[GTP9886-ERR][%s:%d] "fmt"\n", __func__, __LINE__, ##arg)
 #define boot_log(fmt, arg...)	g_info(fmt, ##arg)
-#define ts_debug(fmt, arg...)	do {} while (0)
+#define ts_debug(fmt, arg...)	pr_info("[GTP9886-DBG][%s:%d] "fmt"\n", __func__, __LINE__, ##arg)
+#else
+#define ts_info(fmt, arg...)	pr_debug("[GTP9886-INF][%s:%d] "fmt"\n", __func__, __LINE__, ##arg)
+#define	ts_err(fmt, arg...)		pr_debug("[GTP9886-ERR][%s:%d] "fmt"\n", __func__, __LINE__, ##arg)
+#define boot_log(fmt, arg...)	pr_debug(fmt, ##arg)
+#define ts_debug(fmt, arg...)	pr_debug("[GTP9886-DBG][%s:%d] "fmt"\n", __func__, __LINE__, ##arg)
+#endif
 
 /**
  * goodix_register_ext_module - interface for external module
